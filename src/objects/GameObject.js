3fd7ee97577ca80d28f36c8bbaa824899a2bab3c
ulxsth <ulxsth@gmail.com>
2024-06-08 01:48:00 +0900

    upd: PlayerShip

--- src/objects/GameObject.js
+++ src/objects/GameObject.js
@@ -1,8 +1,9 @@
 export class GameObject {
-  constructor(x, y, width, height) {
+  constructor(x, y, width, height, color) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.color = color;
   }
 }
---
+++ src/objects/PlayerShip.js
@@ -0,0 +1,8 @@
+import { GameObject } from "./GameObject.js";
+
+export class PlayerShip extends GameObject {
+  constructor(x, y, width, height, color) {
+    super(x, y, width, height, color);
+  }
+}
+
