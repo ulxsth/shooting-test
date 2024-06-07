7ed40f3a1947b89bc86d48a6c10a7e583295a5b2
ulxsth <ulxsth@gmail.com>
2024-06-08 02:26:45 +0900

    fix: PlayerBulletにレンダリング情報を追記

--- src/objects/PlayerBullet.js
+++ src/objects/PlayerBullet.js
@@ -1,13 +1,13 @@
-import { BULLET_SPEED } from "../constants.js";
+import { PLAYER_BULLET_SPEED, PLAYER_BULLET_WIDTH, PLAYER_BULLET_HEIGHT, PLAYER_BULLET_COLOR } from "../constants.js";
 import { GameObject } from "./GameObject.js";
 
 export class PlayerBullet extends GameObject {
   constructor(x, y) {
-    super(x, y);
+    super(x, y, PLAYER_BULLET_WIDTH, PLAYER_BULLET_HEIGHT, PLAYER_BULLET_COLOR);
   }
 
   updatePosition() {
-    this.y -= BULLET_SPEED;
+    this.y += PLAYER_BULLET_SPEED;
   }
 }
 
