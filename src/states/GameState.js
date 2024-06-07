05f89ee6304a91fd65af0782e8fd9b545e292e8f
ulxsth <ulxsth@gmail.com>
2024-06-07 18:12:10 +0900

    upd: 射撃関連の関数をGameStateに分離

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,5 +1,5 @@
 export class GameState {
-    constructor() {
+  constructor() {
     this.objects = [
       { type: "player", x: 0, y: 0, width: 40, height: 50, color: "blue" },
     ];
@@ -15,24 +15,24 @@ export class GameState {
     };
   }
 
-// プレイヤー関連
-/**
- * プレイヤーを取得する
- * @returns {Object}
- */
-getPlayerObj = () => this.objects.find((obj) => obj.type === "player");
+  // プレイヤー関連
+  /**
+   * プレイヤーを取得する
+   * @returns {Object}
+   */
+  getPlayerObj = () => this.objects.find((obj) => obj.type === "player");
 
-/**
- * プレイヤーの位置を更新する
- */
-updatePlayerPosition = () => {
-  const player = getPlayerObj();
-  const flags = gameState.getAllFlags();
-  if (flags.up) player.y -= PLAYER_SPEED;
-  if (flags.down) player.y += PLAYER_SPEED;
-  if (flags.left) player.x -= PLAYER_SPEED;
-  if (flags.right) player.x += PLAYER_SPEED;
-}
+  /**
+   * プレイヤーの位置を更新する
+   */
+  updatePlayerPosition = () => {
+    const player = getPlayerObj();
+    const flags = gameState.getAllFlags();
+    if (flags.up) player.y -= PLAYER_SPEED;
+    if (flags.down) player.y += PLAYER_SPEED;
+    if (flags.left) player.x -= PLAYER_SPEED;
+    if (flags.right) player.x += PLAYER_SPEED;
+  };
 
   /**
    * フラグを更新する。
@@ -67,5 +67,45 @@ updatePlayerPosition = () => {
       throw new Error(`Invalid flag name: ${name}`);
     }
   }
-}
 
+  // 弾関連
+  /**
+   * 射撃処理
+   */
+  shoot() {
+    const player = getPlayerObj();
+    const bullet = {
+      type: "playerBullet",
+      x: player.x,
+      y: player.y,
+      width: 10,
+      height: 10,
+      color: "red",
+    };
+    objects.push(bullet);
+  }
+
+  /**
+   * プレイヤーの弾を取得する
+   * @returns {Object[]}
+   */
+  getPlayerBullets = () => objects.filter((obj) => obj.type === "playerBullet");
+
+  /**
+   * 弾の位置を更新する
+   */
+  updateBulletPosition() {
+    const bullets = getPlayerBullets();
+    if (!bullets) return;
+
+    bullets.forEach((bullet) => {
+      bullet.y += BULLET_SPEED;
+
+      // 画面外に出た弾をobjectsから削除
+      // TODO: 計算量が O(N) （Nはすべてのオブジェクト数）なので、パフォーマンスを改善する
+      if (bullet.y < 0) {
+        objects.splice(objects.indexOf(bullet), 1);
+      }
+    });
+  }
+}
