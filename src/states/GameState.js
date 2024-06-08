4c8b644b89354948ae0d16cce3e46aa77ddf34a2
ulxsth <ulxsth@gmail.com>
2024-06-08 09:10:41 +0900

    upd: 重複検知関数の実装

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -1,5 +1,6 @@
 import { PlayerShip } from "../objects/PlayerShip.js";
 import { PlayerBullet } from "../objects/PlayerBullet.js";
+import { EnemyObject } from "../objects/EnemyObject.js";
 
 export class GameState {
   constructor() {
@@ -93,4 +94,49 @@ export class GameState {
       }
     });
   }
+
+  // 敵オブジェクト関連
+  /**
+   * 敵オブジェクトを取得する
+   * @returns {Object[]}
+   */
+  getEnemyObjects = () => this.objects.filter((obj) => obj instanceof EnemyObject);
+
+  /**
+   * PlayerBulletとの衝突判定を行う
+   */
+  checkBulletCollision() {
+    const bullets = this.getPlayerBullets();
+    const enemies = this.getEnemyObjects();
+
+    // TODO: O(N^2) なので、パフォーマンスを改善する
+    bullets.forEach((bullet) => {
+      enemies.forEach((enemy) => {
+        
+      });
+    });
+  }
+
+  /**
+   * 任意の2つのオブジェクトに重なっている部分があるかを検査する。
+   * @param {Object} obj1
+   * @param {Object} obj2
+   * @returns {boolean}
+   */
+  isCollided = (obj1, obj2) => {
+    // 長方形を (x..x+w), (y..y+h) のふたつの範囲として捉え、
+    // 2つのオブジェクトのそれぞれの範囲が重なっているかを考える
+    
+    const isXOverlapped = obj1.x < (obj2.x + obj2.width) &&
+                           (obj1.x + obj1.width) > obj2.x ||
+                           obj2.x < (obj1.x + obj1.width) &&
+                           (obj2.x + obj2.width) > obj1.x;
+
+    const isYOverlapped = obj1.y < (obj2.y + obj2.height) &&
+                           (obj1.y + obj1.height) > obj2.y ||
+                           obj2.y < (obj1.y + obj1.height) &&
+                           (obj2.y + obj2.height) > obj1.y;
+                           
+    return isXOverlapped && isYOverlapped;
+  };
 }
