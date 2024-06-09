86abb2c4267c4543e7840bee7144b3f0dd3c2337
ulxsth <ulxsth@gmail.com>
2024-06-09 15:33:25 +0900

    upd: 左クリックを長押ししている間、インターバルが実行されるようにした

--- src/objects/PlayerShip.js
+++ src/objects/PlayerShip.js
@@ -4,12 +4,14 @@ import {
   PLAYER_SHIP_HEIGHT,
   PLAYER_SHIP_COLOR,
   PLAYER_SPEED,
+  PLAYER_SHOOT_INTERVAL,
 } from "../constants.js";
 import { getCanvasSize } from "../../index.js";
 
 export class PlayerShip extends GameObject {
   constructor(x, y) {
     super(x, y, PLAYER_SHIP_WIDTH, PLAYER_SHIP_HEIGHT, PLAYER_SHIP_COLOR);
+    this.shootIntervalId = null;
   }
 
   updatePosition(flags) {
@@ -26,6 +28,15 @@ export class PlayerShip extends GameObject {
     }
     if (flags.right && this.x + PLAYER_SPEED + PLAYER_SHIP_WIDTH <= canvasWidth) {
       this.x += PLAYER_SPEED;
+    }
+
+    if(flags.leftClick && this.shootIntervalId === null) {
+      this.shootIntervalId = setInterval(() => {
+        console.log("shoot");
+      }, PLAYER_SHOOT_INTERVAL);
+    } else if (!flags.leftClick && this.shootIntervalId !== null) {
+      clearInterval(this.shootIntervalId);
+      this.shootIntervalId = null;
     }
   }
 }
